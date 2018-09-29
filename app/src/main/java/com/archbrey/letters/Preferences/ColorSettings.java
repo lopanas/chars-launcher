754e06963d4e3dee3b86a771bf76d8b248574bf0
Carlos E. Salazar <ce.salazar@gmail.com>
2018-09-29 04:52:32 +0200

    Refactor settings for background color

--- app/src/main/java/com/archbrey/letters/Preferences/ColorSettings.java
+++ app/src/main/java/com/archbrey/letters/Preferences/ColorSettings.java
@@ -1,164 +1,110 @@
 package com.archbrey.letters.Preferences;
 
-
 import android.content.Context;
 import android.content.res.Resources;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-
 import com.archbrey.letters.R;
 
 public class ColorSettings {
-
-    private static GridView mainMenuBox;
-    private static Context settingsContext;
+    private GridView mainMenuBox;
+    private Context settingsContext;
     private static Resources rMainSettings;
 
-
-    public GridView DrawBox (GridView getgridBox,Context c,Resources getR) {
-
-        String[] menuItems;
-
+    public GridView DrawBox(GridView getgridBox, Context c, Resources getR) {
         SettingsActivity.infoView.setText(getR.getString(R.string.color_scheme));
         SettingsActivity.menuArea = getR.getString(R.string.color_scheme);
-        SettingsActivity.menuLevel=1;
+        SettingsActivity.menuLevel = 1;
         mainMenuBox = getgridBox;
         settingsContext = c;
         rMainSettings = getR;
 
-        menuItems = new String[14];
-        menuItems[0] =getR.getString(R.string.dark_1);
-        menuItems[1] =getR.getString(R.string.dark_2);
-        menuItems[2] =getR.getString(R.string.dark_3);
-        menuItems[3] =getR.getString(R.string.dark_4);
-        menuItems[4] =getR.getString(R.string.dark_5);
-        menuItems[5] =getR.getString(R.string.dark_6);
-        menuItems[6] =getR.getString(R.string.dark_7);
-        menuItems[7] =getR.getString(R.string.light_1);
-        menuItems[8] =getR.getString(R.string.light_2);
-        menuItems[9] =getR.getString(R.string.light_3);
-        menuItems[10] =getR.getString(R.string.light_4);
-        menuItems[11] =getR.getString(R.string.light_5);
-        menuItems[12] =getR.getString(R.string.light_6);
-        menuItems[13] =getR.getString(R.string.light_7);
+        // Set the name of the background colors displayed on the menu
+        String[] menuItems = getR.getStringArray(R.array.background_names);
 
         new SettingsDrawer(settingsContext, mainMenuBox, menuItems);
         setListener();
 
         return mainMenuBox;
-
     } //public LinearLayout DrawBox ()
 
-    public void setListener() {mainMenuBox.setOnItemClickListener(new MenuClickListener());}
+
+    public void setListener() {
+        mainMenuBox.setOnItemClickListener(new MenuClickListener());
+    }
 
 
-    private class MenuClickListener implements AdapterView.OnItemClickListener{
-
+    private class MenuClickListener implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-
+            // TODO: there has to be a cleaner approach to this.
             //noinspection SimplifiableIfStatement
             switch (position) {
                 case 0:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.white);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.Black_0);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.Black_2);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","dark1");
+                    setColorScheme(R.color.white, R.color.Black_0, R.color.Black_2, "dark1");
                     break;
                 case 1:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.white);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.Black_2);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.Black_4);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","dark2");
+                    setColorScheme(R.color.white, R.color.Black_2, R.color.Black_4, "dark2");
                     break;
                 case 2:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.white);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.Black_4);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.Black_6);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","dark3");
+                    setColorScheme(R.color.white, R.color.Black_4, R.color.Black_6, "dark3");
                     break;
                 case 3:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.white);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.Black_6);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.Black_8);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","dark4");
+                    setColorScheme(R.color.white, R.color.Black_6, R.color.Black_8, "dark4");
                     break;
                 case 4:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.white);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.Black_8);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.Black_A);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","dark5");
+                    setColorScheme(R.color.white, R.color.Black_8, R.color.Black_A, "dark5");
                     break;
                 case 5:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.white);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.Black_A);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.Black_D);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","dark6");
+                    setColorScheme(R.color.white, R.color.Black_A, R.color.Black_D, "dark6");
                     break;
                 case 6:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.white);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.Black_D);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.Black_F);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","dark7");
+                    setColorScheme(R.color.white, R.color.Black_D, R.color.Black_F, "dark7");
                     break;
                 case 7:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.black);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.White_0);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.White_2);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","light1");
+                    setColorScheme(R.color.black, R.color.White_0, R.color.White_2, "light1");
                     break;
                 case 8:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.black);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.White_2);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.White_4);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","light2");
+                    setColorScheme(R.color.black, R.color.White_2, R.color.White_4, "light2");
                     break;
                 case 9:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.black);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.White_4);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.White_6);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","light3");
+                    setColorScheme(R.color.black, R.color.White_4, R.color.White_6, "light3");
                     break;
                 case 10:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.black);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.White_6);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.White_8);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","light4");
+                    setColorScheme(R.color.black, R.color.White_6, R.color.White_8, "light4");
                     break;
                 case 11:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.black);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.White_8);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.White_A);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","light5");
+                    setColorScheme(R.color.black, R.color.White_8, R.color.White_A, "light5");
                     break;
                 case 12:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.black);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.White_A);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.White_D);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","light6");
+                    setColorScheme(R.color.black, R.color.White_A, R.color.White_D, "light6");
                     break;
                 case 13:
-                    SettingsActivity.textColor = rMainSettings.getColor(R.color.black);
-                    SettingsActivity.backColor = rMainSettings.getColor(R.color.White_D);
-                    SettingsActivity.backerColor = rMainSettings.getColor(R.color.White_F);
-                    SettingsActivity.prefsEditor.putString ("colorscheme","light7");
+                    setColorScheme(R.color.black, R.color.White_D, R.color.White_F, "light7");
                     break;
                 default:
                     break;
             } //switch (position)
 
+            DrawBox(mainMenuBox, settingsContext, rMainSettings);
+        }// public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+
+
+        private void setColorScheme(int textColor, int backColor, int backerColor, String schemeName) {
+            // Set the new colors
+            SettingsActivity.textColor = rMainSettings.getColor(textColor);
+            SettingsActivity.backColor = rMainSettings.getColor(backColor);
+            SettingsActivity.backerColor = rMainSettings.getColor(backerColor);
+            SettingsActivity.prefsEditor.putString("colorscheme", schemeName);
+
+            // Store the settings
             SettingsActivity.SettingChanged = true;
             SettingsActivity.prefsEditor.commit();
-            DrawBox(mainMenuBox, settingsContext, rMainSettings);
-            SettingsActivity.infoBox.setBackgroundColor(SettingsActivity.backerColor);
-            SettingsActivity.infoView.setTextColor(SettingsActivity.textColor);
 
-
-        }// public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            // Change the colors on the current view
+            SettingsActivity.infoBox.setBackgroundColor(backerColor);
+            SettingsActivity.infoView.setTextColor(textColor);
+        }
     } //private class MenuClickListener implements AdapterView.OnItemClickListener
-
-
-
-
 } //public class ColorSettings
